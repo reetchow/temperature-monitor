1b14ee4b0ba3ef9083b8e2e65887962b7acfa8eb
Ubuntu <ubuntu@ip-172-31-40-71.us-east-2.compute.internal>
2019-03-13 04:29:25 +0000

    boilerplate socket.io code

--- server.py
+++ server.py
@@ -1,24 +1,41 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+# Mongo Config
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/temp_database'
 mongo = PyMongo(app) 
+
+# SocketIO Config
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/add', methods=['POST'])
 def add_data():
     if request.method == 'POST':
+        print(request.data)
         data = request.get_json()
-        mongo.db.temp_collection.insert_one(data)
         print(data)
+        mongo.db.temp_collection.insert_one(data)
         return f'Added entry: {data}'
     else:
         return 'Only POSTs accepted'
 
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: '+ str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
 @app.route('/')
-def hello():
-    return 'hello world!!'
+def home_page():
+    return render_template('index.html')
 
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', debug=True)
 
-
+# app.run(host='0.0.0.0')
