bf05955dee5c7fb827494a1b6a3caebb558ea53b
Ubuntu <reetpragya@gmail.com>
2019-04-12 04:04:05 +0000

    added some d3 code for graphing

--- server.py
+++ server.py
@@ -12,30 +12,35 @@ mongo = PyMongo(app)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+# Add a data point into Mongo
 @app.route('/add', methods=['POST'])
 def add_data():
-    if request.method == 'POST':
-        print(request.data)
-        data = request.get_json()
-        print(data)
-        mongo.db.temp_collection.insert_one(data)
-        return f'Added entry: {data}'
-    else:
-        return 'Only POSTs accepted'
+	if request.method == 'POST':
+		print(request.data)
+		data = request.get_json()
+		print(data)
+		mongo.db.temp_collection.insert_one(data)
+		return f'Added entry: {data}'
+	else:
+		return 'Only POSTs accepted'
 
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: '+ str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
+# Provide 100 data points to a new connection
+@socketio.on('connection')
+def on_connection(json, methods=['GET', 'POST']):
+	data_cursor = mongo.db.temp_collection.find().sort('_id', 1)
+	data = []
+	for doc in data_cursor:
+		if doc and len(data) < 100:
+			print(doc)
+			doc.pop('_id')
+			data.append(doc)
+		else:
+			break
+	socketio.emit('init-data', data)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
-
-# app.run(host='0.0.0.0')
+	socketio.run(app, host='0.0.0.0', debug=True)
