acc38e066c4fa361000cb5987111e6467956e4c5
Ubuntu <reetpragya@gmail.com>
2019-05-19 02:46:04 +0000

    using socketio rooms for init-data
    Signed-off-by: Ubuntu <reetpragya@gmail.com>

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ from flask_socketio import SocketIO
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
 # Mongo Config
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/temp_database'
 mongo = PyMongo(app) 
@@ -12,34 +11,37 @@ mongo = PyMongo(app)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+# Connected clients
+users = []
+
 # Add a data point into Mongo
 @app.route('/add', methods=['POST'])
 def add_data():
-	if request.method == 'POST':
-		print(request.data)
-		data = request.get_json()
-		print(data)
-		mongo.db.temp_collection.insert_one(data)
-		return f'Added entry: {data}'
-	else:
-		return 'Only POSTs accepted'
+  if request.method == 'POST':
+    data = request.get_json()
+    print(data)
+    mongo.db.temp_collection.insert_one(data)
+    return f'Added entry: {data}'
+  else:
+    return 'Only POSTs accepted'
 
 # Provide 100 data points to a new connection
 @socketio.on('connection')
 def on_connection(json, methods=['GET', 'POST']):
-	data_cursor = mongo.db.temp_collection.find().sort('time', -1)
-	data = []
-	for doc in data_cursor:
-		if doc and len(data) < 15000:
-			doc.pop('_id')
-			data.append(doc)
-		else:
-			break
-	socketio.emit('init-data', data)
+  users.append(request.sid)
+  data_cursor = mongo.db.temp_collection.find().sort('time', -1)
+  data = []
+  for doc in data_cursor:
+    if doc and len(data) < 150:
+      doc.pop('_id')
+      data.append(doc)
+    else:
+      break
+  socketio.emit('init-data', data, room=request.sid)
 
 @app.route('/')
 def home_page():
-	return render_template('index.html')
+  return render_template('index.html')
 
 if __name__ == '__main__':
-	socketio.run(app, host='0.0.0.0', debug=True)
+  socketio.run(app, host='0.0.0.0', debug=True)
